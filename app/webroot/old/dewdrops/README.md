70ed09c2026afb3c995138ee3966b00445152af7
Sowmya <sowmya.snhu@gmail.com>
2017-12-17 19:51:32 -0500

    moving from v2 openshift
    Signed-off-by: Sowmya <sowmya.snhu@gmail.com>

---
+++ app/webroot/old/dewdrops/README.md
@@ -0,0 +1,13 @@
+# [Dew Drops](http://sowmyakalyan.com/dewdrops) 
+[Dew Drops](http://sowmyakalyan.com/dewdrops) is the personal portfolio website for displaying personal art work of a self-taught artist. This version of the site is based on bootstrap and a carousel theme.
+
+## Creator
+
+[Dew Drops](http://sowmyakalyan.com/dewdrops) is created and maintained by **[Sowmya Kalyan](http://sowmyakalyan.com/)**. The paintings and artwork are a reflection of the emotions and dreams at different times of the journey of life.
+
+* https://github.com/somkal
+
+## Copyright and License
+
+Copyright 2016 Sowmya Kalyan. <br/>
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">&nbsp;Dew Drops</span> by <span xmlns:cc="http://creativecommons.org/ns#" property="cc:attributionName">Sowmya Kalyan</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>. Based on a work at <a xmlns:dct="http://purl.org/dc/terms/" href="https://github.com/somkal/dewdrops" rel="dct:source">https://github.com/somkal/dewdrops</a>.
---
+++ app/webroot/old/dewdrops/js/imgdb.js
@@ -0,0 +1,257 @@
+var imgdb = [{
+    "imgpath": "../paintings/IMG_160912.jpg",
+    "title": "Dumbo Views",
+    "subtitle": "",
+    "date": "Sep 2016"
+},{
+    "imgpath": "../paintings/IMG_160913.jpg",
+    "title": "Blue Skies",
+    "subtitle": "",
+    "date": "Sep 2016"
+},{
+    "imgpath": "../paintings/DSC_0002.JPG",
+    "title": "Sunset in the Village",
+    "subtitle": "Landscape, Oil on Canvas",
+    "date": "Feb 2016"
+}, {
+    "imgpath": "../paintings/DSC_0014.JPG",
+    "title": "Smoke on Fire",
+    "subtitle": "Oil on Board",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0018.JPG",
+    "title": "Making Headlines",
+    "subtitle": "Mixed Media",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0021.JPG",
+    "title": "Ganesha",
+    "subtitle": "First of them all...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0027.JPG",
+    "title": "Robin in the Hood",
+    "subtitle": "Colors",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0031.JPG",
+    "title": "Sunshine Flowers",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0035.JPG",
+    "title": "Misty Meadows",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0038.JPG",
+    "title": "Because of you",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0041.JPG",
+    "title": "Philadelphia Flowers",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0044.JPG",
+    "title": "Embrace the fire in you",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0048.JPG",
+    "title": "Look into my eyes",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0049.JPG",
+    "title": "Honey, I'm Home",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0050.JPG",
+    "title": "Brooklyn Bridge",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0053.JPG",
+    "title": "Krishna",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0056.JPG",
+    "title": "Ganesha",
+    "subtitle": "...",
+    "date": "2013"
+}, {
+    "imgpath": "../paintings/DSC_0058.JPG",
+    "title": "Love Birds",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0062.JPG",
+    "title": "Painting the Night at Paint-night",
+    "subtitle": "",
+    "date": "2014"
+}, {
+    "imgpath": "../paintings/DSC_0064.JPG",
+    "title": "Feathers in my cap",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0068.JPG",
+    "title": "Proud",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0072.JPG",
+    "title": "In search of answers",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0074.JPG",
+    "title": "Rainbow Unicorn",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0077.JPG",
+    "title": "Lines of thoughts",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0078.JPG",
+    "title": "Still Speed",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0079.JPG",
+    "title": "Life is a journey",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0081.JPG",
+    "title": "When I see you",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0082.JPG",
+    "title": "Waiting for answers 2",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0084.JPG",
+    "title": "Now what?",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0085.JPG",
+    "title": "Let it go...",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0091.JPG",
+    "title": "Fall Colors",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0092.JPG",
+    "title": "Where's my food?",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0093.JPG",
+    "title": "Blue Bell",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0095.JPG",
+    "title": "Cutting through the noise",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0011.JPG",
+    "title": "Love is in the rain",
+    "subtitle": "Mixed Media",
+    "date": "2014"
+}, {
+    "imgpath": "../paintings/DSC_0101.JPG",
+    "title": "Nikita",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0109.JPG",
+    "title": "Simplicity",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0110.JPG",
+    "title": "Cover Page",
+    "subtitle": "...",
+    "date": "2015"
+}, {
+    "imgpath": "../paintings/DSC_0020.jpg",
+    "title": "Twinkle",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "../paintings/DSC_0099.jpg",
+    "title": "Ashley",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "../paintings/DSC_0070.jpg",
+    "title": "Pumpkin Spice",
+    "subtitle": "...",
+    "date": "2014 March"
+}, {
+    "imgpath": "../paintings/IMG_0861.jpg",
+    "title": "Carlos",
+    "subtitle": "...",
+    "date": "2013 Aug"
+}, {
+    "imgpath": "../paintings/Photo2281.jpg",
+    "title": "Kenny G",
+    "subtitle": "...",
+    "date": "2013 Apr"
+}, {
+    "imgpath": "../paintings/Photo2130.jpg",
+    "title": "By the lake",
+    "subtitle": "...",
+    "date": "2013 Jan"
+}, {
+    "imgpath": "../paintings/Photo1975.jpg",
+    "title": "Belinda",
+    "subtitle": "...",
+    "date": "2012 Dec"
+}, {
+    "imgpath": "../paintings/Photo1730.jpg",
+    "title": "Home",
+    "subtitle": "...",
+    "date": "2012 Oct"
+}, {
+    "imgpath": "../paintings/Photo1720.jpg",
+    "title": "Cafe Europa",
+    "subtitle": "...",
+    "date": "2012 Oct"
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
